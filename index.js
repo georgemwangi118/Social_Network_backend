1581ae736faadfb8025764a446216cee4dd1d5fd
georgemwangi118 <gmwangi118@yahoo.com>
2022-04-04 00:49:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//importing routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+//DB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+//apiDocs
+app.get("/api", (req, res) => {
+  fs.readFile("docs/apiDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err,
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
+
+// middleware -
+app.use(morgan("start"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized!" });
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const {
+  signup,
+  signin,
+  signout,
+  forgotPassword,
+  resetPassword,
+  socialLogin,
+} = require("../controllers/auth");
+
+//password reset valisator
+const {
+  userSigninValidator,
+  userSignupValidator,
+  passwordResetValidator,
+} = require("../validator");
+const { userById } = require("../controllers/user");
+
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", userSigninValidator, signin);
+router.get("/signout", signout);
+
+//password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
+
+//then use this route for social login
+router.post("/social-login", socialLogin);
+
+//any route containing :userId, our app will first execute userByID()
+router.param("userId", userById);
+
+module.exports = router;
